ea54b08202fd57cf407dc4cba0b6db1ea45f4c49
pbutti <pierfrancesco.butti@protonmail.com>
2023-06-27 17:45:43 +0200

    Tagger in evt display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PhoenixUIModule } from 'phoenix-ui-components';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { LdmxExperimentComponent } from './ldmx-experiment/ldmx-experiment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LdmxExperimentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     RouterModule.forRoot([{ path: '', component: LdmxExperimentComponent }]),
+    BrowserAnimationsModule,
+    PhoenixUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
